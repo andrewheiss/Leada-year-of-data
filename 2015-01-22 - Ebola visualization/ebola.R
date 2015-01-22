515186ace3fe677f2c8b1b420b4c3963fd950f13
Andrew Heiss <andrew@andrewheiss.com>
2015-01-22 12:52:40 -0500

    Add instructions for the rest of the challenge

--- 2015-01-22 - Ebola visualization/ebola.R
+++ 2015-01-22 - Ebola visualization/ebola.R
@@ -44,3 +44,16 @@ ebola <- bind_rows(all.days) %>%
            fatality.rate.for.confirmed.and.probables / 100,
          city = factor(city))
 
+
+#------------------------
+# Step 2: Visualization
+#------------------------
+# Build visualizations for the aggregated data. There are different visualizations you can build (a line graph is a good start). Also, there are different variables you can visualize (Total death/s in confirmed cases, New Case/s (Probable), total confirmed cases, and a lot more). Think about which variables might be interesting / meaningful. You're also free to find additional data (geographical data etc), but make sure you cite your sources!
+ # There are some sample visualizations here: http://www.bbc.com/news/world-africa-28755033
+
+
+#---------------------
+# Step 3: Conclusion
+#---------------------
+# So you got the data and built some visualizations. Now interpret it! Note a few things about the graph. Can you conclude anything? Is the worst over? Maybe it's yet to come? Or not enough data? Let us know your thoughts.
+# Tweet at us with your visualizations! @LeadaHQ
